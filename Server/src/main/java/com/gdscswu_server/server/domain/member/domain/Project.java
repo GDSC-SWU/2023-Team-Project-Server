8dc7179566bc77ebebc6bf4633b061d965eac54d
Yusun Choi <nsuy.ch@gmail.com>
2023-11-24 19:40:34 +0900

    feat: Add Project entity (#1)

---
+++ Server/src/main/java/com/gdscswu_server/server/domain/member/domain/Project.java
@@ -0,0 +1,41 @@
+package com.gdscswu_server.server.domain.member.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Project")
+@Getter
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @NotNull
+    private Member member;
+
+    @NotNull
+    private String title;
+
+    private String type;
+    private String part;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Project project)) return false;
+
+        return Objects.equals(this.id, project.getId()) &&
+                Objects.equals(this.member, project.getMember()) &&
+                Objects.equals(this.title, project.getTitle());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, member, title);
+    }
+}
