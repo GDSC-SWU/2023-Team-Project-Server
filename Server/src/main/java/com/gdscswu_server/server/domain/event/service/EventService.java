0475743de0166cbadae4d08276321f8dddb014a4
Yusun Choi <nsuy.ch@gmail.com>
2023-11-24 19:46:05 +0900

    feat: Add service files (#1)
    - Event, Member, My page, Network

---
+++ Server/src/main/java/com/gdscswu_server/server/domain/event/service/EventService.java
@@ -0,0 +1,7 @@
+package com.gdscswu_server.server.domain.event.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+}
---
+++ Server/src/main/java/com/gdscswu_server/server/domain/mypage/service/MyPageService.java
@@ -0,0 +1,7 @@
+package com.gdscswu_server.server.domain.mypage.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyPageService {
+}
