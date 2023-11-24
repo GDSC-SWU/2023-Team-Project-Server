24b5a5f00e3a55b0bccb5b3819fa0013cfc23406
Yusun Choi <nsuy.ch@gmail.com>
2023-11-24 19:41:17 +0900

    feat: Add Attendance entity (#1)

---
+++ Server/src/main/java/com/gdscswu_server/server/domain/event/domain/Attendance.java
@@ -0,0 +1,40 @@
+package com.gdscswu_server.server.domain.event.domain;
+
+import com.gdscswu_server.server.domain.member.domain.Member;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "Attendance")
+@Getter
+public class Attendance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @NotNull
+    private Member member;
+
+    @ManyToOne
+    @NotNull
+    private Event event;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Attendance attendance)) return false;
+
+        return Objects.equals(this.id, attendance.getId()) &&
+                Objects.equals(this.member, attendance.getMember()) &&
+                Objects.equals(this.event, attendance.getEvent());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, member, event);
+    }
+}
