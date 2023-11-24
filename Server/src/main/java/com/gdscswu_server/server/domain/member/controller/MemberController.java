724c58f48d6f72d3a4645f36c20fb3c600197352
Yusun Choi <nsuy.ch@gmail.com>
2023-11-24 19:45:58 +0900

    feat: Add controller files (#1)
    - Event, Member, My page, Network

---
+++ Server/src/main/java/com/gdscswu_server/server/domain/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.gdscswu_server.server.domain.member.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/member")
+public class MemberController {
+}
