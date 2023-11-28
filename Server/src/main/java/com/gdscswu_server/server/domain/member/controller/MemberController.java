9340516ff3d7cbb2de0002bdd2497f3c092ebd25
Yusun Choi <nsuy.ch@gmail.com>
2023-11-29 01:26:30 +0900

    chore: Add example code for reference (#4)

--- Server/src/main/java/com/gdscswu_server/server/domain/member/controller/MemberController.java
+++ Server/src/main/java/com/gdscswu_server/server/domain/member/controller/MemberController.java
@@ -1,18 +1,26 @@
 package com.gdscswu_server.server.domain.member.controller;
 
+import com.gdscswu_server.server.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-//@RequestMapping("/api/v1/member")
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/member")
 public class MemberController {
+    private final MemberService memberService;
 
-    @GetMapping("golog")
-    public void index() {
-      log.info("들어옴");
+    /*
+    <참고용 예시 코드> spring context에 저장된 member entity 가져오기
+    @GetMapping
+    public ResponseEntity<ResponseDto> test(@AuthenticationPrincipal ContextUser contextUser) {
+        Member member = contextUser.getMember();
+
+        return ResponseEntity.ok(DataResponseDto.of(member, 200));
     }
+     */
+
 }
