e474368a583463718ad215b7f4388bac0101bcf4
Hyjii <hyeon12512@gmail.com>
2023-12-05 23:26:19 +0900

    chore: delete profileUpdate(#8)

--- Server/src/main/java/com/gdscswu_server/server/domain/member/service/MemberService.java
+++ Server/src/main/java/com/gdscswu_server/server/domain/member/service/MemberService.java
@@ -1,8 +1,7 @@
 package com.gdscswu_server.server.domain.member.service;
 
-import com.gdscswu_server.server.domain.member.domain.Member;
 import com.gdscswu_server.server.domain.member.domain.MemberRepository;
-import com.gdscswu_server.server.domain.member.dto.ProfileRequestDto;
+import com.gdscswu_server.server.domain.member.dto.ProfileSaveRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,15 +13,7 @@ public class MemberService {
     private final MemberRepository memberRepository;
 
     @Transactional
-    public Long profileSave(ProfileRequestDto requestDto){
-        return memberRepository.save(requestDto.toEntity()).getId();
-    }
-
-    @Transactional
-    public Long profileUpdate(Long id, ProfileRequestDto requestDto){
-        Member member = memberRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 사용자 없음. id"+ id));
-        member.update(requestDto.getName(), requestDto.getProfileImagePath(), requestDto.getMajor(), requestDto.getAdmissionYear()
-                , requestDto.getEmail(), requestDto.getIntroduction());
-        return id;
+    public Long profileSave(ProfileSaveRequestDto requestDto){
+       return memberRepository.save(requestDto.toEntity()).getId();
     }
 }
