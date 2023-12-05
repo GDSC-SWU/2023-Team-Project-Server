32f34c92036bb439fd6b93dddd9defc379dfcb45
suucong <sophia021210@naver.com>
2023-12-05 10:10:33 +0900

    test: Add Read Member's info, Generation info, Project info API Test Code (#9)

--- Server/src/test/java/com/gdscswu_server/server/domain/member/controller/MemberControllerTest.java
+++ Server/src/test/java/com/gdscswu_server/server/domain/member/controller/MemberControllerTest.java
@@ -1,9 +1,6 @@
 package com.gdscswu_server.server.domain.member.controller;
 
-import com.gdscswu_server.server.domain.member.domain.Generation;
-import com.gdscswu_server.server.domain.member.domain.GenerationRepository;
-import com.gdscswu_server.server.domain.member.domain.Member;
-import com.gdscswu_server.server.domain.member.domain.MemberRepository;
+import com.gdscswu_server.server.domain.member.domain.*;
 import com.gdscswu_server.server.domain.model.TokenClaimVo;
 import com.gdscswu_server.server.global.util.JwtUtil;
 import org.junit.Test;
@@ -12,7 +9,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
@@ -33,6 +29,9 @@ public class MemberControllerTest {
     private GenerationRepository generationRepository;
 
     @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Autowired
@@ -42,43 +41,7 @@ public class MemberControllerTest {
     @Test
     @DisplayName("findById() Test")
     public void findById() throws Exception {
-        // given
-        // 정보를 보고 싶은 멤버
-        Member member = Member.builder()
-                .googleEmail("googleEmail")
-                .name("name")
-                .profileImagePath("profileImagePath")
-                .build();
-        memberRepository.save(member);
-
-        // 정보를 보려는 멤버
-        Member member2 = Member.builder()
-                .googleEmail("googleEmail")
-                .name("name")
-                .profileImagePath("profileImagePath")
-                .build();
-        memberRepository.save(member2);
-
-        TokenClaimVo vo = new TokenClaimVo(member2.getId(), member2.getEmail());
-        String accessToken = jwtUtil.generateToken(true, vo);
-
-        // mockMvc 요청 구성
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/member/{id}", member.getId())
-                .header("Authorization", "Bearer " + accessToken);  // AccessToken 추가
-
-        // mockMvc 실행 및 결과 확인
-        mockMvc.perform(requestBuilder)
-                .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
-                .andDo(print());
-    }
-
-    @Transactional
-    @Test
-    @DisplayName("findGenerationById() Test")
-    public void findGenerationById() throws Exception {
-        // given
-        // 정보를 보고 싶은 멤버
+        // 정보를 보고 싶은 멤버 생성
         Member member = Member.builder()
                 .googleEmail("googleEmail")
                 .name("name")
@@ -104,7 +67,23 @@ public class MemberControllerTest {
                 .build();
         generationRepository.save(generation2);
 
-        // 정보를 보려는 멤버
+        // 정보를 보고 싶은 멤버의 프로젝트 생성
+        Project project = Project.builder()
+                .member(member)
+                .title("project")
+                .generation(1)
+                .build();
+        projectRepository.save(project);
+
+        // 정보를 보고 싶은 멤버의 두번째 프로젝트 생성
+        Project project1 = Project.builder()
+                .member(member)
+                .title("project2")
+                .generation(2)
+                .build();
+        projectRepository.save(project1);
+
+        // 정보를 보려는 멤버 생성
         Member member2 = Member.builder()
                 .googleEmail("googleEmail")
                 .name("name")
@@ -112,18 +91,17 @@ public class MemberControllerTest {
                 .build();
         memberRepository.save(member2);
 
-        // 정보를 보려는 멤버의 액세스 토큰 생성
+        // 정보를 보려는 멤버의 액세스 토큰을 생성
         TokenClaimVo vo = new TokenClaimVo(member2.getId(), member2.getEmail());
         String accessToken = jwtUtil.generateToken(true, vo);
 
-        // mockMvc 요청 구성(헤더에 토큰을 담아 요청을 보냄)
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/member/generation/{id}", member.getId())
-                .header("Authorization", "Bearer " + accessToken);  // AccessToken 추가
+        // mockMvc 요청 구성
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/api/v1/member/{id}", member.getId())
+                .header("Authorization", "Bearer " + accessToken);  // 위에서 생성한 액세스 토큰을 헤더에 추가
 
         // mockMvc 실행 및 결과 확인
         mockMvc.perform(requestBuilder)
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON))
                 .andDo(print());
     }
 }
