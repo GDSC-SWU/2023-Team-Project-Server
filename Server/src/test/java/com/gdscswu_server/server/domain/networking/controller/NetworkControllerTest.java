ef1c1b02fdb6ffb2a1429981522e1045ac2bbb2e
Yusun Choi <nsuy.ch@gmail.com>
2023-12-15 04:05:14 +0900

    refactor: Modify mockMvc URL (#23)

--- Server/src/test/java/com/gdscswu_server/server/domain/networking/controller/NetworkControllerTest.java
+++ Server/src/test/java/com/gdscswu_server/server/domain/networking/controller/NetworkControllerTest.java
@@ -6,9 +6,6 @@ import com.gdscswu_server.server.domain.member.dto.LoginResponseDto;
 import com.gdscswu_server.server.domain.networking.domain.Bookmark;
 import com.gdscswu_server.server.domain.networking.domain.BookmarkRepository;
 import com.gdscswu_server.server.domain.networking.dto.FilterOptionsRequestDto;
-import com.gdscswu_server.server.domain.networking.dto.GenerationResponseDto;
-import com.gdscswu_server.server.domain.networking.dto.MemberResponseDto;
-import com.gdscswu_server.server.domain.networking.dto.ProjectResponseDto;
 import com.gdscswu_server.server.global.util.JwtUtil;
 import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.DisplayName;
@@ -22,16 +19,12 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-import static org.assertj.core.api.ClassBasedNavigableIterableAssert.assertThat;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -54,37 +47,38 @@ class NetworkControllerTest {
     private JwtUtil jwtUtil;
     @Autowired
     private ObjectMapper objectMapper;
+
     @Test
     @DisplayName("전체 (필터링 X) 멤버 조회 테스트")
-    public void findAllMembers() throws Exception{
+    public void findAllMembers() throws Exception {
         // Given
-        Member member1 = memberRepository.save(Member.builder()
+        Member member1 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("abc@abc.com")
                 .name("김슈니")
                 .profileImagePath("path")
                 .build());
 
-        Member member2 = memberRepository.save(Member.builder()
+        Member member2 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("def@def.com")
                 .name("박슈니")
                 .profileImagePath("path")
                 .build());
 
-        Generation generation1=generationRepository.save(Generation.builder()
+        Generation generation1 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(1)
                 .department("Android")
                 .level("Member")
                 .build());
 
-        Generation generation2=generationRepository.save(Generation.builder()
+        Generation generation2 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(2)
                 .department("Server")
                 .level("Core")
                 .build());
 
-        Generation generation3=generationRepository.save(Generation.builder()
+        Generation generation3 = generationRepository.save(Generation.builder()
                 .member(member2)
                 .number(2)
                 .department("Server")
@@ -131,7 +125,7 @@ class NetworkControllerTest {
         final List<String> parts = null;
         final List<String> levels = null;
 
-        FilterOptionsRequestDto filterOptionsRequestDto= FilterOptionsRequestDto.builder()
+        FilterOptionsRequestDto filterOptionsRequestDto = FilterOptionsRequestDto.builder()
                 .parts(departments)
                 .departments(parts)
                 .levels(levels)
@@ -141,17 +135,16 @@ class NetworkControllerTest {
         // 김슈니 박슈니 전부 저장
         generationRepository.saveAll(List.of(generation1, generation2, generation3));
         projectRepository.saveAll(List.of(project1, project2, project3, project4, project5));
-        memberRepository.saveAll(List.of(member1,member2));
+        memberRepository.saveAll(List.of(member1, member2));
         // 북마크는 김슈니만 되어있는 상태
         bookmarkRepository.save(new Bookmark(member1, member1));
 
         // then
-        String url = "http://localhost:" + port + "/api/v1/network";
         LoginResponseDto loginResponseDto = jwtUtil.generateTokens(member1);
 
-        mockMvc.perform(post(url)
+        mockMvc.perform(post("/api/v1/network")
                         .contentType(APPLICATION_JSON).header("Authorization", "Bearer " + loginResponseDto.getAccessToken())
-                .content(new ObjectMapper().writeValueAsString(filterOptionsRequestDto)))
+                        .content(new ObjectMapper().writeValueAsString(filterOptionsRequestDto)))
                 .andExpect(status().isOk())
                 //.andExpect(jsonPath("$[1].name").value("김슈니"))
                 //.andExpect(jsonPath("$[1].memberDetails[0].partList[0].part[0]").value("PM"))
@@ -161,35 +154,35 @@ class NetworkControllerTest {
 
     @Test
     @DisplayName("필터링 멤버 조회 테스트")
-    public void findFilteredMembers() throws Exception{
+    public void findFilteredMembers() throws Exception {
         // Given
-        Member member1 = memberRepository.save(Member.builder()
+        Member member1 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("abc@abc.com")
                 .name("김슈니")
                 .profileImagePath("path")
                 .build());
 
-        Member member2 = memberRepository.save(Member.builder()
+        Member member2 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("def@def.com")
                 .name("박슈니")
                 .profileImagePath("path")
                 .build());
 
-        Generation generation1=generationRepository.save(Generation.builder()
+        Generation generation1 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(1)
                 .department("Android")
                 .level("Member")
                 .build());
 
-        Generation generation2=generationRepository.save(Generation.builder()
+        Generation generation2 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(2)
                 .department("Server")
                 .level("Core")
                 .build());
 
-        Generation generation3=generationRepository.save(Generation.builder()
+        Generation generation3 = generationRepository.save(Generation.builder()
                 .member(member2)
                 .number(2)
                 .department("Server")
@@ -241,7 +234,7 @@ class NetworkControllerTest {
         final List<String> parts = null;
         //final List<String> levels = null;
 
-        FilterOptionsRequestDto filterOptionsRequestDto= FilterOptionsRequestDto.builder()
+        FilterOptionsRequestDto filterOptionsRequestDto = FilterOptionsRequestDto.builder()
                 .parts(parts)
                 .departments(departments)
                 .levels(levels)
@@ -251,15 +244,14 @@ class NetworkControllerTest {
         // 김슈니 박슈니 전부 저장
         generationRepository.saveAll(List.of(generation1, generation2, generation3));
         projectRepository.saveAll(List.of(project1, project2, project3, project4, project5));
-        memberRepository.saveAll(List.of(member1,member2));
+        memberRepository.saveAll(List.of(member1, member2));
         // 북마크는 김슈니만 되어있는 상태
         bookmarkRepository.save(new Bookmark(member1, member1));
 
         // then
-        String url = "http://localhost:" + port + "/api/v1/network";
         LoginResponseDto loginResponseDto = jwtUtil.generateTokens(member1);
 
-        mockMvc.perform(post(url)
+        mockMvc.perform(post("/api/v1/network")
                         .contentType(APPLICATION_JSON).header("Authorization", "Bearer " + loginResponseDto.getAccessToken())
                         .content(new ObjectMapper().writeValueAsString(filterOptionsRequestDto)))
                 .andExpect(status().isOk())
@@ -270,17 +262,16 @@ class NetworkControllerTest {
     }
 
 
-
     @Test
     @DisplayName("멤버 북마크 기능 설정 테스트")
-    public void setBookmark() throws Exception{
+    public void setBookmark() throws Exception {
         //given
-        Member member1 = memberRepository.save(Member.builder()
+        Member member1 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("abc@abc.com")
                 .name("김우동")
                 .profileImagePath("path")
                 .build());
-        Generation generation1=generationRepository.save(Generation.builder()
+        Generation generation1 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(3)
                 .department("Android")
@@ -301,11 +292,10 @@ class NetworkControllerTest {
         // 북마크가 설정되는 걸 테스트 하기 위해 북마크 레포에는 저장하지 않음
 
         Long memberId = member1.getId();
-        String url = "http://localhost:" + port + "/api/v1/network/bookmark/"+memberId;
         LoginResponseDto loginResponseDto = jwtUtil.generateTokens(member1);
 
         // then
-        mockMvc.perform(post(url)
+        mockMvc.perform(post("/api/v1/network/bookmark/" + memberId)
                         .contentType(APPLICATION_JSON).header("Authorization", "Bearer " + loginResponseDto.getAccessToken()))
                 .andExpect(status().isOk())
                 .andDo(print());
@@ -313,14 +303,14 @@ class NetworkControllerTest {
 
     @Test
     @DisplayName("멤버 북마크 기능 해제 테스트")
-    public void removeBookmark() throws Exception{
+    public void removeBookmark() throws Exception {
         //given
-        Member member1 = memberRepository.save(Member.builder()
+        Member member1 = memberRepository.save(Member.googleBuilder()
                 .googleEmail("abc@abc.com")
                 .name("김우동")
                 .profileImagePath("path")
                 .build());
-        Generation generation1=generationRepository.save(Generation.builder()
+        Generation generation1 = generationRepository.save(Generation.builder()
                 .member(member1)
                 .number(3)
                 .department("Android")
@@ -342,11 +332,10 @@ class NetworkControllerTest {
         bookmarkRepository.save(new Bookmark(member1, member1));
 
         Long memberId = member1.getId();
-        String url = "http://localhost:" + port + "/api/v1/network/bookmark/"+memberId;
         LoginResponseDto loginResponseDto = jwtUtil.generateTokens(member1);
 
         // then
-        mockMvc.perform(post(url)
+        mockMvc.perform(post("/api/v1/network/bookmark/" + memberId)
                         .contentType(APPLICATION_JSON).header("Authorization", "Bearer " + loginResponseDto.getAccessToken()))
                 .andExpect(status().isOk())
                 .andDo(print());
